7097fde1c1bb2536797d771b995099d6c7eb130b
Janki547 <janx547@gmail.com>
2026-04-10 19:47:38 -0400

    feat: add team formatting page route

---
+++ src/app/team-formatting/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TeamDashboard } from '@/components/teamFormatting';
+
+/**
+ * Team Formatting Page
+ * Displays the team bias analysis dashboard and learning activities
+ */
+export default function TeamFormattingPage() {
+    return (
+        <main>
+            <TeamDashboard />
+        </main>
+    );
+}
